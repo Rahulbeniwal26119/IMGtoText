d817dfc4bc2628215b54a5b99461a3b5ec0d309c
Rahulbeniwal <rahulbeniwal26119@gmail.com>
2020-08-16 19:58:17 +0530

    adding additional file for understanding

---
+++ source/additional_optional.py
@@ -0,0 +1,13 @@
+import sys
+import glob
+from PIL import Image
+size = 128 , 128 
+
+#convering an image into the thumbnail
+
+for infile in glob.glob('*.jpeg'):
+	file , ext = os.path.splitext(infile)
+	im = Image.open(infile)
+	im.thumbnail(size)
+	im.save(file + ".thumbnail" + "JPEG")
+
---
+++ source/show_loaded_images.py
@@ -0,0 +1,3 @@
+from PIL import Image
+im = Image.open("index.png")
+im.show()
